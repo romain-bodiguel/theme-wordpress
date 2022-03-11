5e374e6b1c2ef87be27ed1b673585c49fe4bba5e
romain-bodiguel <romain.bodiguel@gmail.com>
2022-03-11 12:00:08 +0100

    boucle sur les articles du index.php

--- wordpress/wp-content/themes/apollored/index.php
+++ wordpress/wp-content/themes/apollored/index.php
@@ -23,27 +23,33 @@ get_header();
 //wp_list_categories());
 
 ?>
-
 <main class="right">
     <!-- emmet: h2+article*6>a+h3+div(img+strong+time)+p+a -->
     <h2 class="right__title">Latest News</h2>
-    <div class="posts">
-        <article class="post">
-            <a href="" class="post__category post__category--color-team">team</a>
-            <h3><?= get_the_title(); ?></h3>
-            <div class="post__meta">
-                <img class="post__author-icon" src="/images/icon-dar.png" alt="">
-                <strong class="post__author"><?php the_author(); ?></strong>
-                <time datetime="2018-02-10">le 10 février 2018</time>
-            </div>
-            <p><?php the_content() ?></p>
-            <a href="article.html" class="post__link">Continue reading</a>
-        </article>
-    </div>
+        <div class="posts">
+
+        <?php
+            if (have_posts()) {
+                while (have_posts()) {
+                    the_post(); 
+        ?> 
+            <article class="post">
+                <a href="" class="post__category post__category--color-team">team</a>
+                <h3><?php the_title(); ?></h3>
+                <div class="post__meta">
+                    <img class="post__author-icon" src="../images/icon-dar.png" alt="">
+                    <strong class="post__author"><?php the_author(); ?></strong>
+                    <time datetime="2018-02-10">le 10 février 2018</time>
+                </div>
+                <p><?php the_content(); ?></p>
+                <a href="article.html" class="post__link">Continue reading</a>
+            </article>
+        <?php 
+                }
+            }
+        ?>
+</div>
 </main>
-
-<?php 
-
+<?php
 get_footer();
-
 ?>
